0beb8189265ebdfa93fc6b536111e1a8d9f50d2a
Pedro <pedrocpt2001@gmail.com>
2021-01-16 20:36:37 -0400

    Added classes to represet mazes

---
+++ src/MazeBlock.java
@@ -0,0 +1,11 @@
+public class MazeBlock {
+
+    private int x, y;
+    private SpaceType spaceType;
+
+    public MazeBlock(int x, int y, SpaceType spaceType) {
+        this.x = x;
+        this.y = y;
+        this.spaceType = spaceType;
+    }
+}
