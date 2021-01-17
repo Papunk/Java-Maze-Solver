2191accaeb6ae508aab23c43ca04fdc687507aaa
Pedro <pedrocpt2001@gmail.com>
2021-01-17 15:13:36 -0400

    Built MazeBlock class

--- src/MazeBlock.java
+++ src/MazeBlock.java
@@ -1,11 +1,23 @@
 public class MazeBlock {
 
-    private int x, y;
-    private SpaceType spaceType;
+    private final int x, y;
+    private final SpaceType spaceType;
 
     public MazeBlock(int x, int y, SpaceType spaceType) {
         this.x = x;
         this.y = y;
         this.spaceType = spaceType;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public SpaceType getSpaceType() {
+        return this.spaceType;
     }
 }
